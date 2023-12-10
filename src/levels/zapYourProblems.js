54825a46e3fb454ddc676ae216e02c3d30fd0bc4
ZungrySoft <ZungrySoftEntertainment@gmail.com>
2023-12-10 00:14:49 -0800

    Moved zapYourProblems level up by three units

--- src/levels/zapYourProblems.js
+++ src/levels/zapYourProblems.js
@@ -1,60 +1,60 @@
 export const data = {
     cratesRequired: 4,
     cameraDistance: 8,
-    cameraPosition: [6, 2, 0],
+    cameraPosition: [6, 2, 3],
     cameraStartAngle: [Math.PI*(3/4), Math.PI*(1/8)],
     floorHeight: -8,
     elements: [
         // Main Conveyor
         {
             type: 'conveyor',
-            position: [0, 0, 0],
+            position: [0, 0, 3],
             direction: "east",
             color: 'red',
             scaffold: true,
         },
         {
             type: 'conveyor',
-            position: [1, 0, 0],
+            position: [1, 0, 3],
             direction: "east",
             color: 'red',
             scaffold: true,
         },
         {
             type: 'conveyor',
-            position: [2, 0, 0],
+            position: [2, 0, 3],
             direction: "east",
             color: 'red',
             scaffold: true,
         },
         {
             type: 'conveyor',
-            position: [3, 0, 0],
+            position: [3, 0, 3],
             direction: "east",
             color: 'red',
             scaffold: true,
         },
         {
             type: 'block',
-            position: [4, 0, 1],
+            position: [4, 0, 4],
         },
 
         // Lower Conveyor
         {
             type: 'conveyor',
-            position: [3, 0, -3],
+            position: [3, 0, 0],
             direction: "east",
             color: 'red',
         },
         {
             type: 'conveyor',
-            position: [4, 0, -3],
+            position: [4, 0, 0],
             direction: "east",
             color: 'green',
         },
         {
             type: 'conveyor',
-            position: [5, 0, -3],
+            position: [5, 0, 0],
             direction: "east",
             color: 'green',
         },
@@ -62,19 +62,19 @@ export const data = {
         // Path to sort
         {
             type: 'conveyor',
-            position: [6, 0, -3],
+            position: [6, 0, 0],
             direction: "east",
             color: 'green',
         },
         {
             type: 'conveyor',
-            position: [7, 0, -4],
+            position: [7, 0, -1],
             direction: "east",
             color: 'red',
         },
         {
             type: 'conveyor',
-            position: [8, 0, -4],
+            position: [8, 0, -1],
             direction: "east",
             color: 'red',
         },
@@ -82,19 +82,19 @@ export const data = {
         // Laser Tower
         {
             type: 'crate',
-            position: [9, 0, -1],
+            position: [9, 0, 2],
         },
         {
             type: 'crate',
-            position: [9, 0, 0],
+            position: [9, 0, 3],
         },
         {
             type: 'crate',
-            position: [9, 0, 1],
+            position: [9, 0, 4],
         },
         {
             type: 'laser',
-            position: [9, 0, 2],
+            position: [9, 0, 5],
             direction: "west",
             color: 'green',
         },
@@ -102,7 +102,7 @@ export const data = {
         // Shortener Laser
         {
             type: 'laser',
-            position: [9, 5, -1],
+            position: [9, 5, 2],
             direction: "north",
             color: 'blue',
         },
@@ -110,69 +110,69 @@ export const data = {
         // Sort
         {
             type: 'block',
-            position: [9, 0, -2],
+            position: [9, 0, 1],
         },
         {
             type: 'conveyor',
-            position: [9, 0, -4],
+            position: [9, 0, -1],
             direction: "south",
             color: 'red',
         },
         {
             type: 'conveyor',
-            position: [9, 1, -4],
+            position: [9, 1, -1],
             direction: "south",
             color: 'red',
         },
         {
             type: 'conveyor',
-            position: [9, 2, -4],
+            position: [9, 2, -1],
             direction: "south",
             color: 'red',
         },
         {
             type: 'conveyor',
-            position: [9, 3, -4],
+            position: [9, 3, -1],
             direction: "south",
             color: 'red',
         },
         {
             type: 'conveyor',
-            position: [9, 4, -4],
+            position: [9, 4, -1],
             direction: "south",
             color: 'red',
         },
         {
             type: 'block',
-            position: [9, 5, -3],
+            position: [9, 5, 0],
         },
         {
             type: 'block',
-            position: [9, 5, -2],
+            position: [9, 5, 1],
         },
 
         // Chutes
         {
             type: 'chute',
-            position: [9, 1, -5],
+            position: [9, 1, -2],
             direction: "west",
             letter: 'a',
         },
         {
             type: 'chute',
-            position: [9, 2, -5],
+            position: [9, 2, -2],
             direction: "west",
             letter: 'b',
         },
         {
             type: 'chute',
-            position: [9, 3, -5],
+            position: [9, 3, -2],
             direction: "west",
             letter: 'c',
         },
         {
             type: 'chute',
-            position: [9, 4, -5],
+            position: [9, 4, -2],
             direction: "west",
             letter: 'd',
         },
@@ -180,43 +180,43 @@ export const data = {
         // Laser Platforms
         {
             type: 'block',
-            position: [6, 1, -5],
+            position: [6, 1, -2],
         },
         {
             type: 'block',
-            position: [6, 2, -5],
+            position: [6, 2, -2],
         },
         {
             type: 'block',
-            position: [6, 3, -5],
+            position: [6, 3, -2],
         },
         {
             type: 'block',
-            position: [6, 4, -5],
+            position: [6, 4, -2],
         },
 
         // Lasers
         {
             type: 'laser',
-            position: [6, 1, -4],
+            position: [6, 1, -1],
             direction: "east",
             color: 'yellow',
         },
         {
             type: 'laser',
-            position: [6, 2, -4],
+            position: [6, 2, -1],
             direction: "east",
             color: 'yellow',
         },
         {
             type: 'laser',
-            position: [6, 3, -4],
+            position: [6, 3, -1],
             direction: "east",
             color: 'yellow',
         },
         {
             type: 'laser',
-            position: [6, 4, -4],
+            position: [6, 4, -1],
             direction: "east",
             color: 'yellow',
         },
@@ -224,33 +224,33 @@ export const data = {
         // Crates
         {
             type: 'crate',
-            position: [0, 0, 1],
+            position: [0, 0, 4],
             letter: 'd',
         },
         {
             type: 'crate',
-            position: [0, 0, 2],
+            position: [0, 0, 5],
             letter: 'b',
         },
         {
             type: 'crate',
-            position: [0, 0, 3],
+            position: [0, 0, 6],
             letter: 'c',
         },
         {
             type: 'crate',
-            position: [0, 0, 4],
+            position: [0, 0, 7],
             letter: 'a',
         },
 
         // Leftmost Fan
         {
             type: 'block',
-            position: [8, 0, -2],
+            position: [8, 0, 1],
         },
         {
             type: 'fan',
-            position: [8, 0, -1],
+            position: [8, 0, 2],
             direction: "west",
             color: 'green',
         },
@@ -258,22 +258,22 @@ export const data = {
         // Deco scaffolding
         {
             type: 'scaffold',
-            position: [0, 0, -1],
+            position: [0, 0, 2],
             direction: "east",
         },
         {
             type: 'scaffold',
-            position: [1, 0, -1],
+            position: [1, 0, 2],
             direction: "east",
         },
         {
             type: 'scaffold',
-            position: [1, 0, -2],
+            position: [1, 0, 1],
             direction: "east",
         },
         {
             type: 'scaffold',
-            position: [1, 0, -3],
+            position: [1, 0, 0],
             direction: "east",
         },
     ],
