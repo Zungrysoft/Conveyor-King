38ca5e5f1e420edbc5954c3b1130b6ab60b2087e
ZungrySoft <ZungrySoftEntertainment@gmail.com>
2024-03-12 00:38:13 -0700

    Condensed paint mixing level

--- src/levels/paintMixing2.js
+++ src/levels/paintMixing2.js
@@ -1,18 +1,18 @@
 export const data = {
     cratesRequired: 2,
     cameraDistance: 4,
-    cameraPosition: [2.5, 1, 1],
+    cameraPosition: [1.5, 1, 1],
     cameraStartAngle: [Math.PI*(2/4), Math.PI*(2/8)],
     floorHeight: -5,
     elements: [
         // Right fan
         {
             type: 'block',
-            position: [6, -1, 1],
+            position: [4, -1, 1],
         },
         {
             type: 'fan',
-            position: [6, -1, 2],
+            position: [4, -1, 2],
             color: 'brown',
             direction: 'south',
         },
@@ -20,27 +20,19 @@ export const data = {
         // Chute
         {
             type: 'fan',
-            position: [6, 2, 1],
+            position: [4, 2, 1],
             color: 'brown',
             direction: 'west',
         },
         {
             type: 'fan',
-            position: [5, 2, 0],
+            position: [3, 2, 0],
             color: 'cyan',
             direction: 'west',
         },
         {
             type: 'block',
-            position: [6, 2, 0],
-        },
-        {
-            type: 'block',
-            position: [6, 2, -1],
-        },
-        {
-            type: 'block',
-            position: [5, 2, -1],
+            position: [4, 2, 0],
         },
         {
             type: 'block',
@@ -94,12 +86,12 @@ export const data = {
         // Crates
         {
             type: 'crate',
-            position: [2, 2, 0],
+            position: [1, 2, 0],
             letter: 'a',
         },
         {
             type: 'crate',
-            position: [2, 2, 1],
+            position: [1, 2, 1],
             letter: 'b',
         },
 
@@ -139,31 +131,7 @@ export const data = {
             type: 'conveyor',
             position: [4, 0, 0],
             color: 'white',
-            direction: 'east',
-        },
-        {
-            type: 'conveyor',
-            position: [5, 0, 0],
-            color: 'white',
-            direction: 'east',
-        },
-        {
-            type: 'conveyor',
-            position: [6, 0, 0],
-            color: 'white',
             direction: 'south',
-        },
-        {
-            type: 'conveyor',
-            position: [6, 1, 0],
-            color: 'white',
-            direction: 'west',
-        },
-        {
-            type: 'conveyor',
-            position: [5, 1, 0],
-            color: 'white',
-            direction: 'west',
         },
         {
             type: 'conveyor',
@@ -199,72 +167,72 @@ export const data = {
         // Red Paint
         {
             type: 'conveyor',
-            position: [1, -1, 2],
+            position: [0, -1, 2],
             color: 'red',
             direction: 'south',
         },
         {
             type: 'conveyor',
-            position: [1, -2, 2],
+            position: [0, -2, 2],
             color: 'red',
             direction: 'south',
         },
         {
             type: 'paint',
-            position: [1, -1, 3],
+            position: [0, -1, 3],
             color: 'red',
         },
         {
             type: 'paint',
-            position: [1, -2, 3],
+            position: [0, -2, 3],
             color: 'red',
         },
 
         // Yellow Paint
         {
             type: 'conveyor',
-            position: [2, -1, 2],
+            position: [1, -1, 2],
             color: 'yellow',
             direction: 'south',
         },
         {
             type: 'conveyor',
-            position: [2, -2, 2],
+            position: [1, -2, 2],
             color: 'yellow',
             direction: 'south',
         },
         {
             type: 'paint',
-            position: [2, -1, 3],
+            position: [1, -1, 3],
             color: 'yellow',
         },
         {
             type: 'paint',
-            position: [2, -2, 3],
+            position: [1, -2, 3],
             color: 'yellow',
         },
 
         // Blue Paint
         {
             type: 'conveyor',
-            position: [3, -1, 2],
+            position: [2, -1, 2],
             color: 'blue',
             direction: 'south',
         },
         {
             type: 'conveyor',
-            position: [3, -2, 2],
+            position: [2, -2, 2],
             color: 'blue',
             direction: 'south',
         },
         {
             type: 'paint',
-            position: [3, -1, 3],
+            position: [2, -1, 3],
             color: 'blue',
         },
         {
             type: 'paint',
-            position: [3, -2, 3],
+            position: [2, -2, 3],
             color: 'blue',
         },
     ],
